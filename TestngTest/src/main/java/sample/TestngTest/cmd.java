126bb6f80e9845bec0f36d0bb9ba289c600d53f5
Sharath-Solartis <sharath_n@solartis.net>
2017-06-22 10:21:14 +0530

    commit

---
+++ TestngTest/src/main/java/sample/TestngTest/cmd.java
@@ -0,0 +1,13 @@
+package sample.TestngTest;
+
+import org.testng.annotations.Test;
+
+public class cmd 
+{
+        @Test
+		public void runcmd()
+		{
+			System.out.println("run in cmd");
+		}
+	
+}
