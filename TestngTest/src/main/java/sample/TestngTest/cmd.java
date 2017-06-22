b9a14e769581da2bbc4db79f9a009049cb349c14
Sharath-Solartis <vigneshKumar_p@adsolartis.local>
2017-06-22 17:52:18 +0530

    commit

--- TestngTest/src/main/java/sample/TestngTest/cmd.java
+++ TestngTest/src/main/java/sample/TestngTest/cmd.java
@@ -1,13 +1,16 @@
 package sample.TestngTest;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.Parameters;
 
 public class cmd 
 {
-        @Test
-		public void runcmd()
+	@Parameters("output")
+	@Test 
+	public void runcmd(String output)
 		{
-			System.out.println("run in cmd");
+			System.out.println(System.getProperty("output"));
+			System.out.println(output);
 		}
 	
 }
